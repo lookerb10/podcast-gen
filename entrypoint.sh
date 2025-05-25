546876cc1d30396f72a0e951ce7d399768915c8b
lookerb10 <83416610+lookerb10@users.noreply.github.com>
2025-05-25 16:54:45 +0000

    fixing issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@
 echo "================="
 
 git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.name "${INPUT_EMAIL
+git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /usr/bin/feed.py
+source /venv/bin/activate
+
+python /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
 git push --set-upstream origin main
